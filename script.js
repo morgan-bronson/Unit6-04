b24d5b2f430c3f52b07f7201a38095fe83535782
morganbronson <morgan.bronson@ocsbstudent.ca>
2021-01-05 21:38:57 +0000

    js edits

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 'use strict'
 
 document.getElementById('mybtn').addEventListener('click', museum)
-//Event Listener connects the button with the function "museum()"
+// Event Listener connects the button with the function "museum()"
 
 function museum () {
   const age = (document.getElementById('age').value)
-  //gets age from html document
+  // gets age from html document
   const day = (document.getElementById('day').value)
-  //gets day of week from html document
-  if ((age >=12 && age <=21) || day == 'Tuesday' || day == 'Thursday') {
+  // gets day of week from html document
+  if ((age >= 12 && age <= 21) || day === 'Tuesday' || day === 'Thursday') {
     alert('eligible for student pricing')
-    //makes an alert pop up
-   } else {
+    / /makes an alert pop up
+  } else {
     alert('must pay regular pricing')
-    //makes an alert pop up
+    // makes an alert pop up
   }
 }
