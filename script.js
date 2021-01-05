b9530a29001ec22d8854cab6b3aa3ba75d4fd676
morganbronson <morgan.bronson@ocsbstudent.ca>
2021-01-05 15:08:36 +0000

    added js

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+document.getElementById('mybtn').addEventListener('click', museum)
+
+function museum () {
+  const input = (document.getElementById('input').value)
+  if (age >=12 && <=21) {
+    alert('eligible for student pricing')
+  } else if (day == Tuesday || day == Thursday) {
+    alert('eligible for student pricing')
+  } else 
+  alert('must pay regular pricing')
+  }
+}
