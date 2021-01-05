06c8001b48315b85184c575405459623787982d9
morganbronson <morgan.bronson@ocsbstudent.ca>
2021-01-05 16:21:31 +0000

    added comments

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 'use strict'
 
 document.getElementById('mybtn').addEventListener('click', museum)
+//Event Listener connects the button with the function "museum()"
 
 function museum () {
-  const input = (document.getElementById('input').value)
+  const age = (document.getElementById('age').value)
+  const day = (document.getElementById('day').value)
+  let Tuesday = Date
+  let Thursday = Date
   if (age >=12 && age <=21) {
     alert('eligible for student pricing')
-  } else  if (day == Tuesday || day == Thursday) {
-    alert('eligible for student pricing')   
+    //makes an alert pop up
+  } else if (day == Tuesday || day == Thursday) {
+    alert('eligible for student pricing') 
+    //makes an alert pop up  
   } else {
     alert('must pay regular pricing')
+    //makes an alert pop up
   }
 }
